2edc96fffa0fcd5aa1e847539dabf342cbe3c972
Guy <guy.lud@gmail.com>
2017-12-27 00:03:06 +0200

    start working on PostgresqlDapperImplementor

---
+++ ExistsForAll.DapperExtensions.Postgresql/IPostgresqlDapperImplementor.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace ExistsForAll.DapperExtensions.Postgresql
+{
+	public interface IPostgresqlDapperImplementor : IDapperImplementor
+	{
+		void Upsert<T>(IDbConnection connection, T entity, IDbTransaction transaction, int? commandTimeout) where T : class;
+	}
+}
---
+++ ExistsForAll.DapperExtensions.Postgresql/PostgresqlDapperImplementor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using ExistsForAll.DapperExtensions.Mapper;
+using ExistsForAll.DapperExtensions.Sql;
+
+namespace ExistsForAll.DapperExtensions.Postgresql
+{
+	public class PostgresqlDapperImplementor : DapperImplementor , IPostgresqlDapperImplementor
+	{
+		public PostgresqlDapperImplementor(ISqlGenerator sqlGenerator,
+			IClassMapperRepository classMappers,
+			IDapperExtensionsConfiguration dapperExtensionsConfiguration) 
+			: base(sqlGenerator, classMappers, dapperExtensionsConfiguration)
+		{
+		}
+
+		public void Upsert<T>(IDbConnection connection, T entity, IDbTransaction transaction, int? commandTimeout) where T : class
+		{
+			var classMap = ClassMappers.GetMap<T>();
+			var keyPredicate = classMap.GetKeyPredicate(entity);
+
+			var parameters = new Dictionary<string, object>();
+			var dynamicParameters = new DynamicParameters();
+		}
+
+	}
+}
