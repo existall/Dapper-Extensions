3638d9aa608209e114383777b7b1fb3a7faf15a1
Guy <guy.lud@gmail.com>
2017-12-28 23:34:20 +0200

    b

---
+++ ExistsForAll.DapperExtensions/IUpdateActionsProvider.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using ExistsForAll.DapperExtensions.Mapper;
+using ExistsForAll.DapperExtensions.Predicates;
+using ExistsForAll.DapperExtensions.Sql;
+
+namespace ExistsForAll.DapperExtensions
+{
+	internal interface IUpdateActionsProvider
+	{
+		ActionParams Update(IClassMapper classMap, IPredicate predicate, IList<IProjectionSet> projectionSets);
+		ActionParams Update(IClassMapper classMap, object entity);
+	}
+
+	internal class UpdateActionsProvider : GeneratorBase, IUpdateActionsProvider
+	{
+		public UpdateActionsProvider(IDapperExtensionsConfiguration configuration,
+			IClassMapperRepository classMapperRepository,
+			ISqlGenerator sqlGenerator)
+			: base(configuration, classMapperRepository, sqlGenerator)
+		{
+		}
+
+		public ActionParams Update(IClassMapper classMap, IPredicate predicate, IList<IProjectionSet> projectionSets)
+		{
+			var parameters = new Dictionary<string, object>();
+			var dynamicParameters = new DynamicParameters();
+
+			var sql = SqlGenerator.Update(classMap, predicate, projectionSets, parameters);
+
+			foreach (var parameter in parameters)
+			{
+				dynamicParameters.Add(parameter.Key, parameter.Value);
+			}
+
+			return ActionParams.New(sql, dynamicParameters);
+		}
+
+		public ActionParams Update(IClassMapper classMap, object entity)
+		{
+			var predicate = classMap.GetKeyPredicate(entity);
+			var parameters = new Dictionary<string, object>();
+			var sql = SqlGenerator.Update(classMap, predicate, parameters);
+			var dynamicParameters = new DynamicParameters();
+
+			var columns = classMap.GetMutableColumns();
+
+			foreach (var property in ReflectionHelper.GetObjectValues(entity)
+				.Where(property => columns.Any(c => c.Name == property.Key)))
+			{
+				dynamicParameters.Add(property.Key, property.Value);
+			}
+
+			foreach (var parameter in parameters)
+			{
+				dynamicParameters.Add(parameter.Key, parameter.Value);
+			}
+
+			return ActionParams.New(sql, dynamicParameters);
+		}
+	}
+}
---
+++ ExistsForAll.DapperExtensions/MultiActionParams.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Dapper;
+
+namespace ExistsForAll.DapperExtensions
+{
+	internal struct MultiActionParams
+	{
+		public string Sql { get; }
+		public IList<DynamicParameters> DynamicParameterses { get; }
+
+		public MultiActionParams(string sql, IList<DynamicParameters> dynamicParameterses)
+		{
+			Sql = sql;
+			DynamicParameterses = dynamicParameterses;
+		}
+
+		public static MultiActionParams New(string sql, IList<DynamicParameters> dynamicParameterses)
+		{
+			return new MultiActionParams(sql, dynamicParameterses);
+		}
+	}
+}
